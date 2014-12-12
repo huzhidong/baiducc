aa75e4b258a48faa076b2300b38e8a25f65a6e7e
U-WINDOWS-979I7D5\Administrator <Administrator@bchenyuzhen.internal.baidu.com>
2014-12-12 18:32:07 +0800

    update agentbar compile ent to vs2010

--- tools/agentbar/clean.bat
+++ tools/agentbar/clean.bat
@@ -1,17 +1,14 @@
 
 if exist output rd /q /s output
 if not exist output mkdir output
-set CMD_VC2003="c:/Program Files/Microsoft Visual Studio .NET 2003/Common7/IDE/Devenv"
 
-chmod +x bin/*.exe
+set CMD_VS2010="%VS100COMNTOOLS%/../IDE/Devenv"
 
 rem "build image plugin "
-%CMD_VC2003% "ccl_imgex\ccl_imgex.vcproj" /clean Release /project ccl_imgex
+%CMD_VS2010% "ccl_imgex\ccl_imgex.vcxproj" /clean Release /project ccl_imgex
 
 rem "build main activex "
-%CMD_VC2003% "agentbarsrc\CCAgentBar.vcproj" /clean Release /project CCAgentBar
-%CMD_VC2003% "agentbarsrc\CCAgentBar.vcproj" /clean Debug /project CCAgentBar
+%CMD_VS2010% "agentbarsrc\CCAgentBar.vcxproj" /clean Release /project CCAgentBar
 
 rem "build test project"
-%CMD_VC2003% "agentbartest\CCAgentBarTest.vcproj" /clean Debug /project CCAgentBarTest
-%CMD_VC2003% "agentbartest\CCAgentBarTest.vcproj" /clean Release /project CCAgentBarTest
+%CMD_VS2010% "agentbartest\CCAgentBarTest.vcxproj" /clean Release /project CCAgentBarTest
