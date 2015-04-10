f80db44d489045009d0c474d66dbb691f2bfcadc
prog.chen <prog.chen@gmail.com>
2015-04-09 23:55:59 -0700

    update bgcc version

--- dep/bgcc/build.sh
+++ dep/bgcc/build.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SRCTAR=v2.2.6.tar.gz
+SRCTAR=v2.2.7.tar.gz
 
 
 if [ -f "output/include/bgcc.h" -a -f "output/include/openssl/ssl.h" -a -f "output/lib/libbgcc.a" -a -f "output/lib/libssl.a" ]; then
@@ -22,15 +22,15 @@ if [ -f "output/include/bgcc.h" -a -f "output/include/openssl/ssl.h" -a -f "outp
 fi
 
 if [ ! -f ${SRCTAR} ]; then
-    curl --location-trusted -k -O https://github.com/BaiduCC/BGCC/archive/v2.2.6.tar.gz
+    curl --connect-timeout 10 --location-trusted -k -O https://github.com/BaiduCC/BGCC/archive/${SRCTAR}
 fi
 
-if [ ! -f ${SRCTAR} ]; then
+if [ $? != 0 -o ! -f ${SRCTAR} ]; then
     echo "can't find ${SRCTAR}, build failed";
     exit 1;
 fi
 
-tar xzf ${SRCTAR} && make -C BGCC-2.2.6 && cp -r BGCC-2.2.6/output ./ && exit 0
+tar xzf ${SRCTAR} && make -C BGCC-2.2.7 && cp -r BGCC-2.2.7/output ./ && exit 0
 
 
 exit 1
